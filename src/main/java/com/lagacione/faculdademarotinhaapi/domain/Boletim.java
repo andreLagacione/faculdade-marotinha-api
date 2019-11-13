df21175450b66ecef8b82ad81101fe83b126aa4c
André Lagacione <lagacioneandre@gmail.com>
2019-11-13 16:33:24 -0300

    Boletim class

--- src/main/java/com/lagacione/faculdademarotinhaapi/domain/Boletim.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/domain/Boletim.java
@@ -1,4 +1,11 @@
 package com.lagacione.faculdademarotinhaapi.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "boletim")
 public class Boletim {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
