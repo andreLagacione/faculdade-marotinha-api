acf31ad69f1648097c459ea54460544a2856bce3
andreLagacione <lagacioneandre@gmail.com>
2019-11-18 21:21:40 -0300

    create new DTO

--- src/main/java/com/lagacione/faculdademarotinhaapi/dto/CursoNomeListaDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/dto/CursoNomeListaDTO.java
@@ -1,4 +1,33 @@
 package com.lagacione.faculdademarotinhaapi.dto;
 
+import com.lagacione.faculdademarotinhaapi.domain.Curso;
+
 public class CursoNomeListaDTO {
+    private Integer id;
+    private String nome;
+
+    public CursoNomeListaDTO() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static CursoNomeListaDTO of(Curso curso) {
+        CursoNomeListaDTO cursoNomeListaDTO = new CursoNomeListaDTO();
+        cursoNomeListaDTO.setId(curso.getId());
+        cursoNomeListaDTO.setNome(curso.getName());
+        return cursoNomeListaDTO;
+    }
 }
