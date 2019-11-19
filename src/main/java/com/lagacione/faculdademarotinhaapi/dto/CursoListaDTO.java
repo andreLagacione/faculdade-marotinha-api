14eacb0fcf686e0299cd1b74458d9f2969744abd
André Lagacione <lagacioneandre@gmail.com>
2019-11-19 17:17:05 -0300

    create new dtos

--- src/main/java/com/lagacione/faculdademarotinhaapi/dto/CursoListaDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/dto/CursoListaDTO.java
@@ -1,4 +1,33 @@
 package com.lagacione.faculdademarotinhaapi.dto;
 
+import com.lagacione.faculdademarotinhaapi.domain.Curso;
+
 public class CursoListaDTO {
+    private Integer id;
+    private String nome;
+
+    public CursoListaDTO() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id=id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    public static CursoListaDTO of(Curso curso) {
+        CursoListaDTO cursoListaDTO = new CursoListaDTO();
+        cursoListaDTO.setId(curso.getId());
+        cursoListaDTO.setNome(curso.getName());
+        return cursoListaDTO;
+    }
 }
--- src/main/java/com/lagacione/faculdademarotinhaapi/dto/CursoToEditDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/dto/CursoToEditDTO.java
@@ -1,4 +1,48 @@
 package com.lagacione.faculdademarotinhaapi.dto;
 
+import com.lagacione.faculdademarotinhaapi.domain.Curso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CursoToEditDTO {
+    private Integer id;
+    private String nome;
+    private List<Integer> idMaterias = new ArrayList<>();
+
+    public CursoToEditDTO() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id=id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    public List<Integer> getIdMaterias() {
+        return idMaterias;
+    }
+
+    public void setIdMaterias(List<Integer> idMaterias) {
+        this.idMaterias=idMaterias;
+    }
+
+    public static CursoToEditDTO of(Curso curso) {
+        CursoToEditDTO cursoToEditDTO = new CursoToEditDTO();
+        cursoToEditDTO.setId(curso.getId());
+        cursoToEditDTO.setNome(curso.getName());
+        List<Integer> materias = curso.getMaterias().stream().map(materia -> materia.getId()).collect(Collectors.toList());
+        cursoToEditDTO.setIdMaterias(materias);
+        return cursoToEditDTO;
+    }
 }
