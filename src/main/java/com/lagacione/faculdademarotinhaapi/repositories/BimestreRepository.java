edfe6eb6287f6bf4eff0dc65388ccd8e43d3d91f
andreLagacione <lagacioneandre@gmail.com>
2019-11-24 12:02:32 -0300

    refactor, remove classes for create a new Boletim

--- src/main/java/com/lagacione/faculdademarotinhaapi/repositories/BimestreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lagacione.faculdademarotinhaapi.repositories;
-
-import com.lagacione.faculdademarotinhaapi.domain.Bimestre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BimestreRepository extends JpaRepository<Bimestre, Integer> {
-}
