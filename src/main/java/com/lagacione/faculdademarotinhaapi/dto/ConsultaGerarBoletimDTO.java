7f03e4d3c6b12df434d6ee96f3e407a78b41eb39
andreLagacione <lagacioneandre@gmail.com>
2019-11-24 11:25:18 -0300

    new DTO for filter generate boletim

--- src/main/java/com/lagacione/faculdademarotinhaapi/dto/ConsultaGerarBoletimDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/dto/ConsultaGerarBoletimDTO.java
@@ -1,4 +1,48 @@
 package com.lagacione.faculdademarotinhaapi.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class ConsultaGerarBoletimDTO {
+
+    @NotNull(message = "Informe o ano desejado")
+    private Integer ano;
+
+    @NotNull(message = "Selecione o aluno")
+    private Integer alunoId;
+
+    @NotNull(message = "Selecione o curso")
+    private Integer cursoId;
+
+    public ConsultaGerarBoletimDTO() {}
+
+    public ConsultaGerarBoletimDTO(Integer ano, Integer alunoId, Integer cursoId) {
+        this.ano = ano;
+        this.alunoId = alunoId;
+        this.cursoId = cursoId;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getAlunoId() {
+        return alunoId;
+    }
+
+    public void setAlunoId(Integer alunoId) {
+        this.alunoId = alunoId;
+    }
+
+    public Integer getCursoId() {
+        return cursoId;
+    }
+
+    public void setCursoId(Integer cursoId) {
+        this.cursoId = cursoId;
+    }
 }
