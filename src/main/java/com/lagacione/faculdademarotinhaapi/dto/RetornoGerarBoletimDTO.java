c67d769a0c43b62e608751b6b5c364e30894eac3
andreLagacione <lagacioneandre@gmail.com>
2019-12-02 22:33:15 -0300

    create new DTO to return generated boletim id

--- src/main/java/com/lagacione/faculdademarotinhaapi/dto/RetornoGerarBoletimDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/dto/RetornoGerarBoletimDTO.java
@@ -1,4 +1,20 @@
 package com.lagacione.faculdademarotinhaapi.dto;
 
-public class RetornoGerarBoletim {
+import org.springframework.http.HttpStatus;
+
+public class RetornoGerarBoletimDTO extends PadraoMensagemRetornoDTO {
+    private String id;
+
+    public RetornoGerarBoletimDTO(HttpStatus httpStatus, Integer httpStatusCode, String mensagem, String id) {
+        super(httpStatus, httpStatusCode, mensagem);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
