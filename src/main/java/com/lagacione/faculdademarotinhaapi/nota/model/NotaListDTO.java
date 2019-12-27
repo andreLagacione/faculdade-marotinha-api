e0b17e597b7a46824cf020e02282fa459a7a408e
André Lagacione <lagacioneandre@gmail.com>
2019-12-27 14:47:33 -0300

    change type data

--- src/main/java/com/lagacione/faculdademarotinhaapi/nota/model/NotaListDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/nota/model/NotaListDTO.java
@@ -3,10 +3,10 @@ package com.lagacione.faculdademarotinhaapi.nota.model;
 public class NotaListDTO {
     private Integer id;
     private String nomeMateria;
-    private Double notaBimestre1;
-    private Double notaBimestre2;
-    private Double notaBimestre3;
-    private Double notaBimestre4;
+    private String notaBimestre1;
+    private String notaBimestre2;
+    private String notaBimestre3;
+    private String notaBimestre4;
     private String mediaFinal = "N/A";
 
     public NotaListDTO() {}
@@ -27,35 +27,35 @@ public class NotaListDTO {
         this.nomeMateria = nomeMateria;
     }
 
-    public Double getNotaBimestre1() {
+    public String getNotaBimestre1() {
         return notaBimestre1;
     }
 
-    public void setNotaBimestre1(Double notaBimestre1) {
+    public void setNotaBimestre1(String notaBimestre1) {
         this.notaBimestre1 = notaBimestre1;
     }
 
-    public Double getNotaBimestre2() {
+    public String getNotaBimestre2() {
         return notaBimestre2;
     }
 
-    public void setNotaBimestre2(Double notaBimestre2) {
+    public void setNotaBimestre2(String notaBimestre2) {
         this.notaBimestre2 = notaBimestre2;
     }
 
-    public Double getNotaBimestre3() {
+    public String getNotaBimestre3() {
         return notaBimestre3;
     }
 
-    public void setNotaBimestre3(Double notaBimestre3) {
+    public void setNotaBimestre3(String notaBimestre3) {
         this.notaBimestre3 = notaBimestre3;
     }
 
-    public Double getNotaBimestre4() {
+    public String getNotaBimestre4() {
         return notaBimestre4;
     }
 
-    public void setNotaBimestre4(Double notaBimestre4) {
+    public void setNotaBimestre4(String notaBimestre4) {
         this.notaBimestre4 = notaBimestre4;
     }
 
