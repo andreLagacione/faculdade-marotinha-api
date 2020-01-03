264c19f434454b625c73244e72091267d1b60c44
André Lagacione <lagacioneandre@gmail.com>
2020-01-03 11:50:24 -0300

    change propereties

--- src/main/java/com/lagacione/faculdademarotinhaapi/boletim/model/BoletimFilter.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/boletim/model/BoletimFilter.java
@@ -1,45 +1,25 @@
 package com.lagacione.faculdademarotinhaapi.boletim.model;
 
-import com.lagacione.faculdademarotinhaapi.aluno.entity.Aluno;
-import com.lagacione.faculdademarotinhaapi.professor.entity.Professor;
-import com.lagacione.faculdademarotinhaapi.turma.entity.Turma;
-
 public class BoletimFilter {
 
     private Integer ano;
-    private Professor professor;
-    private Aluno aluno;
-    private Turma turma;
+    private Integer idProfessor;
+    private Integer idAluno;
+    private Integer idTurma;
 
     public Integer getAno() {
         return ano;
     }
 
-    public void setAno(Integer ano) {
-        this.ano = ano;
+    public Integer getIdProfessor() {
+        return idProfessor;
     }
 
-    public Professor getProfessor() {
-        return professor;
+    public Integer getIdAluno() {
+        return idAluno;
     }
 
-    public void setProfessor(Professor professor) {
-        this.professor = professor;
-    }
-
-    public Aluno getAluno() {
-        return aluno;
-    }
-
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-
-    public Turma getTurma() {
-        return turma;
-    }
-
-    public void setTurma(Turma turma) {
-        this.turma = turma;
+    public Integer getIdTurma() {
+        return idTurma;
     }
 }
