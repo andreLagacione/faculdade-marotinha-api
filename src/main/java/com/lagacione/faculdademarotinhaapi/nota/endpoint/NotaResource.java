6e8243d65b25fcbdc76543a2ad1e6f9a029d7b81
andreLagacione <lagacioneandre@gmail.com>
2020-01-07 22:01:30 -0300

    change http method

--- src/main/java/com/lagacione/faculdademarotinhaapi/nota/endpoint/NotaResource.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/nota/endpoint/NotaResource.java
@@ -32,7 +32,7 @@ public class NotaResource {
         return this.notaService.findAll();
     }
 
-    @PostMapping(value="/id-boletim/{idBoletim}")
+    @GetMapping(value="/id-boletim/{idBoletim}")
     public List<NotaListDTO> findByBoletimId(@PathVariable Integer idBoletim) {
         return this.notaService.findByBoletimId(idBoletim);
     }
