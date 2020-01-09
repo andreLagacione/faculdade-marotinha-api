2410ae1322030964e6f8b84dd28c61762f3ac23b
André Lagacione <lagacioneandre@gmail.com>
2020-01-09 13:25:45 -0300

    add Lombok Pessoa module

--- src/main/java/com/lagacione/faculdademarotinhaapi/pessoa/entity/Pessoa.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/pessoa/entity/Pessoa.java
@@ -1,15 +1,21 @@
 package com.lagacione.faculdademarotinhaapi.pessoa.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Pessoa {
+
     private String name;
     private Integer age;
     private String cpf;
     private Long phone;
-
-    public Pessoa() {}
 
     public Pessoa(String name, Integer age, String cpf, Long phone) {
         this.name = name;
@@ -18,35 +24,4 @@ public class Pessoa {
         this.phone = phone;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(Long phone) {
-        this.phone = phone;
-    }
 }
