3d2cffae870a0691c1b9f6ecc692760f16acc1e2
andreLagacione <lagacioneandre@gmail.com>
2020-01-17 18:49:58 -0300

    add lombock

--- src/main/java/com/lagacione/faculdademarotinhaapi/commons/models/PadraoMensagemRetornoDTO.java
+++ src/main/java/com/lagacione/faculdademarotinhaapi/commons/models/PadraoMensagemRetornoDTO.java
@@ -1,42 +1,18 @@
 package com.lagacione.faculdademarotinhaapi.commons.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PadraoMensagemRetornoDTO {
 
     private HttpStatus httpStatus;
     private Integer httpStatusCode;
     private String message;
-
-    public PadraoMensagemRetornoDTO() {}
-
-    public PadraoMensagemRetornoDTO(HttpStatus httpStatus, Integer httpStatusCode, String message) {
-        this.httpStatus = httpStatus;
-        this.httpStatusCode = httpStatusCode;
-        this.message = message;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public void setHttpStatus(HttpStatus httpStatus) {
-        this.httpStatus = httpStatus;
-    }
-
-    public Integer getHttpStatusCode() {
-        return httpStatusCode;
-    }
-
-    public void setHttpStatusCode(Integer httpStatusCode) {
-        this.httpStatusCode = httpStatusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String mensagem) {
-        this.message = message;
-    }
 }
